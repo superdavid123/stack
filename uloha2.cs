1324a6ee8792201e37332646fd78f9c3060ce849
superdavid123 <168566456+superdavid123@users.noreply.github.com>
2025-02-04 17:42:34 +0100

    Update uloha2.cs

--- uloha2.cs
+++ uloha2.cs
@@ -1,1 +1,42 @@
 
+Console.WriteLine("vložte závorky");
+string zavorky = Console.ReadLine();
+char[] poleZnaku = zavorky.ToCharArray();
+Stack<char> zasobnik = new Stack<char>();
+
+void chyba()
+{
+	Console.WriteLine("závorky nejsou v pořádku");
+	Environment.Exit(1);
+}
+
+foreach (char znak in poleZnaku)
+{
+	if (znak == '(' || znak == '[' || znak == '{')
+	{
+		zasobnik.Push(znak);
+	}
+	else
+	{
+		if (znak == ')' || znak == '}' || znak == ']')
+		{
+			if (zasobnik.Count == 0)
+			{
+				chyba();
+			}
+			char top = zasobnik.Pop();
+			if ((znak == ')' && top != '(')
+				|| (znak == ']' && top != '[')
+				|| (znak == '}' && top != '{')
+				) { chyba(); }
+		}
+		else { chyba(); }
+
+	}
+}
+if (zasobnik.Count != 0)
+{
+	chyba();
+}
+
+Console.WriteLine("závorky v pořádku");
